c54925fc9e5df84898f9e38675e42e2e0e935e17
a@a.ru <a@a.ru>
2019-02-23 20:23:41 +0300

    Code refactor: added ansible as the deployment hypervisor

--- setup/main.tf
+++
@@ -1,8 +0,0 @@
-module "backend" {
-  source = "../modules/backend"
-
-  bootstrap      = "${terraform.workspace == "base" ? 1 : 0}"
-  bucket         = "${var.bucket}"
-  dynamodb_table = "${var.dynamodb_table}"
-  prefix         = "${var.prefix}"
-}
