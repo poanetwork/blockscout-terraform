3721a247b0c6e2f2a84edc4799e45ad1bbb02fb9
Arsenii Petrovich <arsenii.petrovich@altoros.com>
2019-02-26 17:33:46 +0300

    Fix destroy resources

--- roles/main_infra/files/deploy.tf
+++ roles/main_infra/files/deploy.tf
@@ -1,6 +1,7 @@
 resource "aws_s3_bucket" "explorer_releases" {
   bucket = "${var.prefix}-explorer-codedeploy-releases"
   acl    = "private"
+  force_destroy = "true"
 
   versioning {
     enabled = true
